0d0ffe5c9d2fa25c9d75d917d294189c8213eb66
Keyle Diaz <140049370+KeyleTherese31@users.noreply.github.com>
2025-04-10 18:20:47 +0800

    backend update

--- LabExam/blog/urls.py
+++ LabExam/blog/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
+from django.contrib import admin 
 from . import views
 from django.contrib.auth import views as auth_views
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('post/<int:post_id>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_create, name='post_create'),
