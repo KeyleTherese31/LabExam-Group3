626fc75f2f13e6471d201e6bdc21b96ff7d61d5f
Janella Lorque <163000607+jangggg29@users.noreply.github.com>
2025-04-10 17:47:45 +0800

    Update views.py

--- LabExam/blog/views.py
+++ LabExam/blog/views.py
@@ -1,1 +1,56 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Post
+from .forms import PostForm
+from django.contrib.auth import login
+from .forms import RegisterForm
 
+def home(request):
+    posts = Post.objects.all().order_by('-created_at')
+    return render(request, 'blog/home.html', {'posts': posts})
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+@login_required
+def post_create(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            new_post = form.save(commit=False)
+            new_post.author = request.user
+            new_post.save()
+            return redirect('post_detail', post_id=new_post.id)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_create.html', {'form': form})
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = RegisterForm()
+    return render(request, 'blog/register.html', {'form': form})
+
+@login_required
+def post_edit(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+
+    # Ensure only the author can edit the post
+    if post.author != request.user:
+        return redirect('home')  # You can redirect to a 'permission denied' page instead if needed
+
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('post_detail', post_id=post.id)
+    else:
+        form = PostForm(instance=post)
+
+    return render(request, 'blog/post_edit.html', {'form': form, 'post': post})
